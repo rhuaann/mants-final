8b9a3437a37548acbd7a2809c2d49a7ba2227d55
rhuaann <rhuanlucastm1234@gmail.com>
2023-12-15 13:42:42 -0300

    dockerizado

--- main/settings.py
+++ main/settings.py
@@ -25,9 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b_8rjz%rq-ged2i$v8m*(%pf-z@dmg93t57r1j4o=2tq*s!)(u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = ['http://localhost:81']
 
 
 # Application definition
@@ -102,8 +106,12 @@ EMAIL_HOST_PASSWORD = 'pbxp depd pnro ywwi'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    "ENGINE": "django.db.backends.postgresql",
+    "NAME": "mants",
+    "USER": "postgres",
+    "PASSWORD": "aluno@pf",
+    "HOST": "postgres",
+    "PORT": "5432",
     }
 }
 
@@ -148,8 +156,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = str(BASE_DIR / "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / "static/images/media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
